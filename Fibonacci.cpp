15e2eb7883c3da39a2f34d25df6b788762492b64
ShivaaniShri <devigurumoorthy1982@gmail.com>
2021-08-02 19:00:03 +0530

    Adding Fibonacci.cpp file in my repository
    The examples for Fibonacci series

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -7,6 +7,8 @@ int Fibonacci_Function_One(int number){
     int fib[number_one];
     fib[0] = 0;
     fib[1] = 1;
+    cout<<"Fibonacci series by dynamic programming "<<endl;
+    cout<<fib[0]<<" "<<fib[1]<<" ";
     for (int i = 2;i<=number_one;i++){
         fib[i] = fib[i-1] + fib[i-2];
         cout<<fib[i]<<" ";
@@ -27,8 +29,8 @@ int main(){
     Fibonacci_Function_One(value);
     cout<<endl;
     Fibonacci_Function_Two(value);
-    cout<<endl;
+    cout<<"Fibonacci series by recursion function method"<<endl;
     for (int i = 0;i<=value;i++){
-        cout<<Fibonacci_Function_Two(i)<<endl;
+        cout<<Fibonacci_Function_Two(i)<<" ";
     }
 }
